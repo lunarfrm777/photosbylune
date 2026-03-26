1e54f95e7f7b857a9ac9a9512d0de1ddef092cb9
lunarfrm777 <photoslune8@gmail.com>
2026-03-26 11:45:22 -0400

    Massive Commit: Photographer component to Gallery,, updated imports,, styling completed to have 2 cards per row

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    disableDevTools: true, // ✅ top-level, disables the Astro dev toolbar
+  },
+  vite: {
+    server: {
+      fs: {
+        allow: [
+          '.', // allow current project root
+          'C:/Users/dreym/node_modules', // allow global node_modules if needed
+        ],
+      },
+    },
+  },
+});
