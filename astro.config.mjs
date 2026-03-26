4776cb4853307ed52d206d7f67f2f6a672ab8596
lunarfrm777 <photoslune8@gmail.com>
2026-03-26 11:53:16 -0400

    removed experimental key in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from 'astro/config';
+// astro.config.mjs
 
 export default defineConfig({
-  experimental: {
-    disableDevTools: true, // ✅ top-level, disables the Astro dev toolbar
-  },
   vite: {
     server: {
       fs: {
         allow: [
-          '.', // allow current project root
-          'C:/Users/dreym/node_modules', // allow global node_modules if needed
+          '.', 
+          'C:/Users/dreym/node_modules',
         ],
       },
     },
