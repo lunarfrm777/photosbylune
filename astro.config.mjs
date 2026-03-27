6c7a067fd104e15c12fd83c5a98feeff7fa135fc
lunarfrm777 <photoslune8@gmail.com>
2026-03-27 16:00:11 -0400

    installed tailwind to then install DAISYUSI!!! AHHHHH found it again :3 my fav <3 brought in carousel!!!!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 // astro.config.mjs
 
 export default defineConfig({
@@ -11,5 +12,7 @@ export default defineConfig({
         ],
       },
     },
+
+    plugins: [tailwindcss()],
   },
 });
